d3e56b7a48fa5ff2d8f05b73037c3cc4a7c86ad3
Kanahiro <mediterranean1769@gmail.com>
2020-12-01 15:23:34 +0900

    change img

--- gtfs_viewer.py
+++ gtfs_viewer.py
@@ -166,7 +166,8 @@ class GTFSViewer:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = os.path.join(os.path.dirname(__file__), 'imgs', 'icon.png')
+        icon_path = os.path.join(os.path.dirname(
+            __file__), 'imgs', 'busstop.png')
         self.add_action(
             icon_path,
             text=self.tr(u'GTFS Viewer'),
