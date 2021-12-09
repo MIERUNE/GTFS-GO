5f84b5c8c85b6030f8b9247196c2048f68cdffe6
Kanahiro <mediterranean1769@gmail.com>
2021-12-09 15:43:49 +0900

    unittest, ci

--- gtfs_parser/tests/test_gtfs_parser.py
+++ gtfs_parser/tests/test_gtfs_parser.py
@@ -1,7 +1,6 @@
 import os
 import unittest
 import glob
-import tempfile
 
 from gtfs_parser.__main__ import GTFSParser  # nopep8
 
@@ -42,20 +41,63 @@ class TestGtfsParser(unittest.TestCase):
     def test_read_stops(self):
         # list of geojson-feature
         self.assertEqual(899, len(self.gtfs_parser.read_stops()))
-        # num of features is not changed by aggregation
+        # num of stops is not changed by aggregation
         self.assertEqual(899, len(self.gtfs_parser_frequency.read_stops()))
+        # num of stops is not changed by as_unify_stops
         self.assertEqual(
             899, len(self.gtfs_parser_frequency_unify.read_stops()))
+
         # remove no-route stops
         self.assertEqual(
             896, len(self.gtfs_parser.read_stops(ignore_no_route=True)))
 
     def test_read_routes(self):
-        # num of features read_routes() outputs correspond to routes defined in routes.txt
+        # num of features in routes.geojson depends on not shapes.txt but routes.txt
         self.assertEqual(32, len(self.gtfs_parser.read_routes()))
         self.assertEqual(32, len(self.gtfs_parser.read_routes(no_shapes=True)))
+        # as_frequency and as_unify make no effect to read_routes()
+        self.assertEqual(
+            32, len(self.gtfs_parser_frequency.read_routes(no_shapes=True)))
+        self.assertEqual(
+            32, len(self.gtfs_parser_frequency_unify.read_routes(no_shapes=True)))
 
+    def test_read_interpolated_stops(self):
+        with self.assertRaises(AttributeError):
+            # read_interpolated_stops() needs as_frequency=True
+            self.gtfs_parser.read_interpolated_stops()
+
+        # read_interpolated_stops unify stops having same lat-lon into one featrure.
+        # there are no stops having same lat-lon in fixture
+        self.assertEqual(
+            899, len(self.gtfs_parser_frequency.read_interpolated_stops()))
+
+        # as_unify means near and similar named stops move into same lat-lon(centroid of them)
+        self.assertEqual(
+            518, len(self.gtfs_parser_frequency_unify.read_interpolated_stops()))
+
+    def test_read_route_frequency(self):
+        with self.assertRaises(KeyError):
+            self.gtfs_parser.read_route_frequency()
+
+        # each route_frequency feature is drawn between 2 stops
         self.assertEqual(
             956, len(self.gtfs_parser_frequency.read_route_frequency()))
+        # unify some 'similar' stops into same position, this decrease num of route_frequency features
         self.assertEqual(
             918, len(self.gtfs_parser_frequency_unify.read_route_frequency()))
+
+        # out of service of GTFS -> 0
+        self.assertEqual(0, len(
+            self.gtfs_parser_frequency_unify.read_route_frequency(yyyymmdd="20210530")))
+
+        # some routes are not in service on 20210730, Friday
+        freq20210730 = self.gtfs_parser_frequency_unify.read_route_frequency(
+            yyyymmdd="20210730")
+        self.assertEqual(916, len(freq20210730))
+        self.assertEqual(114, freq20210730[0]["properties"]["frequency"])
+
+        # 20210801 - Sunday
+        freq20210801 = self.gtfs_parser_frequency_unify.read_route_frequency(
+            yyyymmdd="20210801")
+        self.assertEqual(736, len(freq20210801))
+        self.assertEqual(62, freq20210801[0]["properties"]["frequency"])
